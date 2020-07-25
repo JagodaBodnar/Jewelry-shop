aac9e19ccc160dc97a65172a6c6b0cc47f97964d
Jagoda Bodnar <jagodabodnar90@gmail.com>
2020-07-25 23:31:12 +0200

    ContentfulKeyApi correction

--- src/data/contentfulData.js
+++ src/data/contentfulData.js
@@ -2,5 +2,5 @@ import * as contentful from "contentful";
 
 export const client = contentful.createClient({
   space: "fov8s1zxsda6",
-  accessToken: process.env.CONTENTFUL_ID,
+  accessToken: `${process.env.API_KEY}`,
 });
