14d5245ea974d4feec762a66e72187444021244f
Jagoda Bodnar <jagodabodnar90@gmail.com>
2020-07-25 21:07:55 +0200

    ContentfulData change key

--- src/data/contentfulData.js
+++ src/data/contentfulData.js
@@ -2,5 +2,5 @@ import * as contentful from "contentful";
 
 export const client = contentful.createClient({
   space: "fov8s1zxsda6",
-  accessToken: "PuJ09_LUoYBFfJWYgz20KAsPpUFDmucyvjtDIikMh1Y",
+  accessToken: process.env.CONTENTFUL_ID,
 });
